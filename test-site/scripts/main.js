0b5c36e528629a5099f137ff2f3a50bb6d37f9bf
yiduspace <160831841+yiduspace@users.noreply.github.com>
2024-02-22 15:50:20 +0800

    Add files via upload

---
+++ test-site/scripts/main.js
@@ -0,0 +1,26 @@
+let myImage = document.querySelector("img");
+
+myImage.onclick = function () {
+  let mySrc = myImage.getAttribute("src");
+  if (mySrc === "images/firefox-icon.png") {
+    myImage.setAttribute("src", "images/firefox2.png");
+  } else {
+    myImage.setAttribute("src", "images/firefox-icon.png");
+  }
+};
+
+let myButton = document.querySelector("button");
+let myHeading = document.querySelector("h1");
+
+function setUserName() {
+  let myName = prompt("请输入你的名字。");
+  if (!myName) {
+    setUserName();
+  } else {
+    localStorage.setItem("name", myName);
+    myHeading.textContent = "Mozilla 酷毙了，" + myName;
+  }
+}
+myButton.onclick = function () {
+  setUserName();
+};
